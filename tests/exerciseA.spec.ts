e8981c687556542b3d6e54c6799263aba76f1b62
adrianalvarado0815 <adrian.alvarado.fdz@gmail.com>
2026-06-20 12:03:13 -0600

    First playwright excersice using Codegen

---
+++ tests/exerciseA.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('agents abilities', async ({ page }) => {
+  await page.goto('https://playvalorant.com/es-mx/');
+  await page.getByTestId('riotbar:desktopNav:link-AGENTES').click();
+  await page.getByRole('button', { name: 'Cypher' }).click();
+  await page.getByTestId('icon-tab-tab-1').click();
+  await page.getByText('CÁMARA ESPÍA', { exact: true }).dblclick();
+  await page.getByTestId('icon-tab-tab-2').click();
+  await page.getByTestId('icon-tab-tab-3').click();
+  await page.goto('https://playvalorant.com/es-mx/agents/');
+  await page.getByRole('button', { name: 'Deadlock' }).click();
+  await page.getByTestId('icon-tab-tab-1').click();
+  await page.getByTestId('icon-tab-tab-2').click();
+  await page.getByTestId('icon-tab-tab-3').click();
+});
